54ccf73be89f7f91fc31394e92bf4a3fe8649d1b
Joshua Tipton <72083305+JoshuaTipton@users.noreply.github.com>
2021-01-05 01:13:58 +0000

    Created Solution

---
+++ EntertainmentCentreProgram/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int userChoice;
+
+	cout << "+--------------------+" << endl;
+	cout << "|ENTERTAINMENT CENTRE|" << endl;
+	cout << "+--------------------+" << endl;
+
+	cout << "1. Animations" << endl;
+	cout << "7. Exit" << endl;
+
+	cin >> userChoice;
+
+	return 0;
+}
