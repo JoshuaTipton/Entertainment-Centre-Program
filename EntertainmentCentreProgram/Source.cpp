b78835d278294c8182b880cb4c37227d756a2f0b
Joshua Tipton <72083305+JoshuaTipton@users.noreply.github.com>
2021-05-12 15:34:35 +0100

    Updated ECP
    gaming moment

--- EntertainmentCentreProgram/Source.cpp
+++ EntertainmentCentreProgram/Source.cpp
@@ -1,18 +1,33 @@
+#include <string>
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int userChoice;
+	
+	do
+	{
+		cout << "+--------------------+" << endl;
+		cout << "|ENTERTAINMENT CENTRE|" << endl;
+		cout << "+--------------------+" << endl;
 
-	cout << "+--------------------+" << endl;
-	cout << "|ENTERTAINMENT CENTRE|" << endl;
-	cout << "+--------------------+" << endl;
+		cout << "0. Exit" << endl;
+		cout << "1. Quote of the Day" << endl;
 
-	cout << "1. Animations" << endl;
-	cout << "7. Exit" << endl;
+		cin >> userChoice;
 
-	cin >> userChoice;
-
-	return 0;
+		switch (userChoice)
+		{
+		case 0:
+			system("CLS");
+			cout << "bye-bye!\n";
+			return 0;
+		case 1:
+			system("CLS");
+			cout << "animations deez nuts, gottem!\n";
+			break;
+		}
+	}
+	while (userChoice != 0);
 }
